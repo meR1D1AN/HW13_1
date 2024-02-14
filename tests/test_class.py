2f226672f5c274b5fc4b1ac58ecc00dba417c34f
mer1d1an <nikita.sh@gklifts.ru>
2024-02-14 21:46:19 +0300

    покрыл больше тестов

--- tests/test_class.py
+++ tests/test_class.py
@@ -1,19 +1,18 @@
 from cl.category import Category
 from cl.product import Product
 
-import pytest
-
 
 def test_category_initialization():
-    category = Category("Тест категории", "Тест описания")
-    assert category.name == "Тест категории"
+    category = Category("Тест названия", "Тест описания")
+    assert category.name == "Тест названия"
     assert category.description == "Тест описания"
+    assert category.products == []
 
 
 def test_product_initialization():
-    product = Product("Smartphone", "Latest model", 500.0, 10)
-    assert product.name == "Smartphone"
-    assert product.description == "Latest model"
+    product = Product("Тест названия", "Тест описания", 500.0, 10)
+    assert product.name == "Тест названия"
+    assert product.description == "Тест описания"
     assert product.quantity == 500.0
     assert product.price == 10
 
@@ -30,3 +29,11 @@ def test_count_categories():
     category1 = Category("Категория 1", "Описание 1")
     category2 = Category("Категория 2", "Описание 2")
     assert Category.total_categories == initial_categories_count + 2
+
+
+def test_add_product_to_category():
+    category = Category("Тест категории", "Тест описания категории")
+    product = Product("Тест продукта", "Тест описания продукта", 500.0, 10)
+    category.add_product(product)
+    assert len(category.products) == 1
+    assert category.products[0] == product
