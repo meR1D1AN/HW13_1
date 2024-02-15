83f1fa4be10c0924a0677a976d46e5950070d4be
meR1D1AN <156647259+meR1D1AN@users.noreply.github.com>
2024-02-15 06:15:47 +0300

    Update category.py

--- cl/category.py
+++ cl/category.py
@@ -5,7 +5,7 @@ class Category:
     total_categories = 0
     total_unique_products = 0
 
-    def __init__(self, name, description, ):
+    def __init__(self, name, description):
         self.name = name
         self.description = description
         self.products = []
